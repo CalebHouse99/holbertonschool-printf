97abc43ef9efa2ef030cdec93cc25be77a41a8b9
PStewart1 <landofalcon007@gmail.com>
2022-07-05 17:03:28 -0500

    added IF in _puts for a null string

--- test/main.c
+++ test/main.c
@@ -15,20 +15,25 @@ int main(void)
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
-     printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+	_printf("Null character:[%c]\n", NULL);	
+	printf("Null character:[%c]\n", NULL);
+	len = _printf("Null string:[%s]\n", NULL);
+	len2 = printf("Null string:[%s]\n", NULL);
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len, len2);
+/*    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
+*/
 /*    ui = (unsigned int)INT_MAX + 1024; 
      addr = (void *)0x7ffe637541f0;
     _printf("Unsigned:[%u]\n", ui);
